af8a5328f23209f1110447ad0de37dfb13af5332
Фкеуь <dzalaevartem@MacBook-Air-dzalaev.local>
2023-05-09 23:01:58 +0300

    🎉 Begin a project

---
+++ querendo/Model/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  querendo
+//
+//  Created by Давид Джалаев on 08.05.2023.
+//
+
+import Foundation
---
+++ querendo/querendoApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct querendoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
